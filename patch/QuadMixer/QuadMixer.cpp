d65a0451cb4fcb0695b4f9da766b727b314c6a97
Erwin Coumans <erwincoumans@google.com>
2020-08-09 14:26:22 -0700

    example of proper sprintf, using https://github.com/mpaland/printf memory grows around 7300 bytes

--- patch/QuadMixer/QuadMixer.cpp
+++ patch/QuadMixer/QuadMixer.cpp
@@ -6,13 +6,23 @@ using namespace daisysp;
 
 DaisyPatch patch;
 
+float ctrlVal[4];
+namespace test {
+#include "printf.h"
+#include "printf.c"
+};
+char buf[64];
+
 static void AudioCallback(float **in, float **out, size_t size)
 {
-    float ctrlVal[4];
     for (int i = 0; i < 4; i++)
     {
 	//Get the four control values
 	ctrlVal[i] = patch.controls[i].Process();
+        if (ctrlVal[i]<0.003)
+           ctrlVal[i] = 0;
+        if (ctrlVal[i]>0.97)
+           ctrlVal[i]=1;
     }
     
     for (size_t i = 0; i < size; i++)
@@ -35,6 +45,26 @@ static void AudioCallback(float **in, float **out, size_t size)
     }
 }
 
+void UpdateOled()
+{
+    patch.display.Fill(false);
+
+    //test::sprintf(buf,"%s", "QuadMixer");
+    //patch.display.SetCursor(0,0);
+    //patch.display.WriteString(buf, Font_6x8, true);
+
+    for (int i=0;i<4;i++)
+    {
+      //two circuits
+      patch.display.SetCursor(0, i*10);
+      test::sprintf(buf, "ctrl%d:%1.5f", i, ctrlVal[i]);
+      patch.display.WriteString(buf, Font_7x10, true);
+    }
+
+    patch.display.Update();
+}
+
+
 int main(void)
 {
     patch.Init(); // Initialize hardware (daisy seed, and patch)
@@ -47,6 +77,7 @@ int main(void)
     while(1)
     {
 	//display the four control levels
-	patch.DisplayControls(false);
+	//patch.DisplayControls(false);
+        UpdateOled();
     }
 }
